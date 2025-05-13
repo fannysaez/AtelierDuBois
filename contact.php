ecb030255918f7caefcfb0cddad18d70f41ff5cf
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-05-13 10:51:06 +0200

    mise à jour contact.php

--- contact.php
+++ contact.php
@@ -1,17 +1,30 @@
 <?php include 'partials/header.php'; ?>
+
 <main>
-    <h2>Contactez-nous</h2>
-    <form action="#" method="post">
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="name" required>
-        
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required>
-        
-        <label for="message">Message :</label>
-        <textarea id="message" name="message" required></textarea>
-        
-        <button type="submit">Envoyer</button>
-    </form>
+    <section class="contact flex flex-column items-center">
+
+        <h1 class="text-center">Contactez-nous</h1>
+
+        <form class="flex flex-column gap-10" action="#" method="post">
+            <div class="flex flex-column gap-3">
+                <label for="name">Nom :</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+
+            <div class="flex flex-column gap-3">
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+
+            <div class="flex flex-column gap-3">
+                <label for="message">Message :</label>
+                <textarea id="message" name="message" rows="10" required></textarea>
+            </div>
+
+            <button class="btn btn-primary" type="submit">Envoyer</button>
+        </form>
+
+    </section>
 </main>
+
 <?php include 'partials/footer.php'; ?>
